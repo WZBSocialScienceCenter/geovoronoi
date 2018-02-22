d90c65a665b1f4ebef0bb3563fdf53427183105e
Markus Konrad <markus.konrad@wzb.eu>
2018-02-22 15:15:52 +0100

    finalized first example

---
+++ README.md
@@ -0,0 +1,9 @@
+# geovoronoi – a package to create and plot Voronoi regions in geographic areas 
+
+## Limitations
+
+* MultiPolygon shapes of geographic areas are not treated separately when cutting the Voronoi regions (see Sardinia in `random_points_across_italy.py` example)
+
+## TODO
+
+* better support of MultiPolygon shapes of geographic areas (see Sardinia in `random_points_across_italy.py` example)
--- examples/random_points_across_italy.py
+++ examples/random_points_across_italy.py
@@ -13,31 +13,39 @@ tmtoolkit_log = logging.getLogger('geovoronoi')
 tmtoolkit_log.setLevel(logging.INFO)
 tmtoolkit_log.propagate = True
 
-N_POINTS = 80
+N_POINTS = 100
+COUNTRY = 'Italy'
 
 np.random.seed(123)
 
+print('loading country `%s` from naturalearth_lowres' % COUNTRY)
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-
-area = world[world.name == 'Germany']
+area = world[world.name == COUNTRY]
 assert len(area) == 1
 
-print('area CRS:', area.crs)   # gives epsg:4326 -> WGS 84
+print('CRS:', area.crs)   # gives epsg:4326 -> WGS 84
 
 area = area.to_crs(epsg=3395)    # convert to World Mercator CRS
-area_shape = area.iloc[0].geometry
+area_shape = area.iloc[0].geometry   # get the Polygon
 
+# generate some random points within the bounds
 minx, miny, maxx, maxy = area_shape.bounds
 
 randx = np.random.uniform(minx, maxx, N_POINTS)
 randy = np.random.uniform(miny, maxy, N_POINTS)
 coords = np.vstack((randx, randy)).T
-pts = [p for p in coords_to_points(coords) if p.within(area_shape)]
+
+# use only the points inside the geographic area
+pts = [p for p in coords_to_points(coords) if p.within(area_shape)]  # converts to shapely Point
+
 print('will use %d of %d randomly generated points that are inside geographic area' % (len(pts), N_POINTS))
-coords = points_to_coords(pts)
+coords = points_to_coords(pts)   # convert back to simple NumPy coordinate array
 
 del pts
 
+#
+# calculate the Voronoi regions, cut them with the geographic area shape and assign the points to them
+#
 
 poly_shapes, pts, poly_to_pt_assignments = voronoi_regions_from_coords(coords, area_shape)
 
@@ -48,6 +56,10 @@ poly_shapes, pts, poly_to_pt_assignments = voronoi_regions_from_coords(coords, a
 fig, ax = subplot_for_map()
 
 plot_voronoi_polys_with_points_in_area(ax, area_shape, poly_shapes, coords, poly_to_pt_assignments)
-#plot_voronoi_polys_with_points_in_area(ax, area_shape, poly_shapes, coords)
+#plot_voronoi_polys_with_points_in_area(ax, area_shape, poly_shapes, coords)   # monocolor
 
+ax.set_title('%d random points and their Voronoi regions in %s' % (len(pts), COUNTRY))
+
+plt.tight_layout()
+plt.savefig('random_points_across_italy.png')
 plt.show()
