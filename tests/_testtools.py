204e6abb0d047fecf84fec8e92f766a50a928bb9
Markus Konrad <markus.konrad@wzb.eu>
2020-03-05 14:56:58 +0100

    integrated tox raised requirements in setup.py

--- tests/_testtools.py
+++ tests/_testtools.py
@@ -10,6 +10,6 @@ real_coords_2d = partial(st.lists, elements=real_floats(), min_size=2, max_size=
 
 
 def coords_2d_array():
-    return st_numpy.arrays(np.float_, st_numpy.array_shapes(2, 2),
+    return st_numpy.arrays(np.float_, st_numpy.array_shapes(min_dims=2, max_dims=2),
                            elements=st.floats(allow_nan=False, allow_infinity=False))\
                     .filter(lambda arr: arr.shape[1] == 2)
